44a09c4dcab39b96d1fdbfedff7a9032c0367d2a
Andrew Mcveigh <me@andrewmcveigh.com>
2016-12-08 19:04:32 +0000

    Start parsing with spec

--- src/flak/type.clj
+++ src/flak/type.clj
@@ -43,6 +43,47 @@
 (type Vector    vector?)
 (type Map       map?)
 
-(data Maybe (or Nothing (Just a)))
+(s/def ::type-name
+  (s/and symbol? #(re-matches #"^[A-Z][A-Za-z]*$" (c/name %))))
 
-(data Either (or (Left a) (Right b)))
+(s/def ::type-parameter
+  (s/and symbol? #(re-matches #"^[a-z]$" (c/name %))))
+
+(s/def ::type-args
+  (s/+ (s/or ::type-name ::type-name ::type-parameter ::type-parameter)))
+
+(s/def ::product-constructor
+  (s/cat ::type-name ::type-name ::type-args ::type-args))
+
+(s/def ::type-constructor
+  (s/or ::type-name ::type-name ::type-args ::product-constructor))
+
+(s/def ::sum-constructor
+  (s/cat :or #(= % 'or) ::type-constructor (s/+ ::type-constructor)))
+
+(s/def ::data-constructor (s/or ::type-constructor ::sum-constructor))
+
+(s/conform ::sum-constructor '(or Nothing (Just a)))
+
+(s/def ::data-type
+  (s/cat ::type-constructor ::type-constructor
+         ::data-constructor ::data-constructor))
+
+(s/conform ::data-type '[(Maybe a) (or Nothing (Just a))])
+
+(data (Either a b) (or (Left a) (Right b)))
+(data (Point Int Int) (Point x y))
+
+;;; We want a way to program completely dynamically, just like Clojure, but
+;;; without the yucky java types.
+;;; You wouldn't want to cast, or destructure types all the time. You'd want
+;;; a more dynamic protocol/multimethod based dispatch.
+;;; But you'd also want a type-fixed way to program. Which would be much like
+;;; Haskell but with Lispyness, syntax, dependent (spec like) types, etc.
+;;; The boundary between static and dynamic code is really important. There
+;;; should be a very easy but strict way to blend the two.
+;;; Something like a forced handler that casts to correct/incorrect.
+
+;;; The collections protocols are a think of beauty, we want that
+
+;;; But also ADTs, Functors, Monads, etc.

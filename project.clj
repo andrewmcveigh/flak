aedc4a7f290a63e0c9cf8756dca185a35dfd7d7b
Andrew Mcveigh <me@andrewmcveigh.com>
2017-02-21 14:04:12 +0000

    Add manual classpath scripts

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject flak "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/test.check "0.9.0"]
+                 [com.cemerick/pomegranate "0.3.1"]])
+
